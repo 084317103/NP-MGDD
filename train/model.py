3b1c76fc0302f47ef1c8673d9c6be673a650c4e0
084317103 <50193282+084317103@users.noreply.github.com>
2024-10-11 00:21:10 +0800

    Update model.py

--- train/model.py
+++ train/model.py
@@ -26,11 +26,6 @@ class GPTConfig:
         for k,v in kwargs.items():
             setattr(self, k, v)
             
-class GPT1Config(GPTConfig):
-    """ GPT-1 like network roughly 125M params """
-    n_layer = 12
-    n_head = 12
-    n_embd = 768
     
 class CausalSelfAttention(nn.Module):
     """

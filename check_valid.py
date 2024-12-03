c6a561d109f809a14082ba15a3405f061d878c52
084317103 <50193282+084317103@users.noreply.github.com>
2024-12-03 10:35:20 +0800

    Update check_valid.py

--- check_valid.py
+++ check_valid.py
@@ -1,10 +1,16 @@
 from rdkit import Chem
-input_file = "autodl-tmp/MolGPT/datasets/zuhe/cmaup.txt"
-output_file = "autodl-tmp/MolGPT/datasets/zuhe/cmaup_after.txt"
-
-with open(input_file, "r") as infile, open(output_file, "w") as outfile:
-    for line in infile:
-        smiles = line.strip()
-        mol = Chem.MolFromSmiles(smiles)
-        if mol is not None:
-            outfile.write(smiles + "\n")
+input_files = [
+    "NPMGDD/datasets/zuhe/npass.txt",
+    "NPMGDD/datasets/zuhe/tcmbank.txt",
+    "NPMGDD/datasets/zuhe/inflamnat.txt",
+    "NPMGDD/datasets/zuhe/cmaup.txt"
+]
+for input_file in input_files:
+    output_file = input_file.replace(".txt", "_after.txt")
+    
+    with open(input_file, "r") as infile, open(output_file, "w") as outfile:
+        for line in infile:
+            smiles = line.strip()
+            mol = Chem.MolFromSmiles(smiles)
+            if mol is not None:
+                outfile.write(smiles + "\n")

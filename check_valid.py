f4a88664a49dc52c05dd4b3bd8cd12a7505ccac2
084317103 <50193282+084317103@users.noreply.github.com>
2024-12-03 11:29:40 +0800

    Update check_valid.py

--- check_valid.py
+++ check_valid.py
@@ -1,10 +1,14 @@
 from rdkit import Chem
 input_files = [
-    "NPMGDD/datasets/zuhe/npass.txt",
-    "NPMGDD/datasets/zuhe/tcmbank.txt",
-    "NPMGDD/datasets/zuhe/inflamnat.txt",
-    "NPMGDD/datasets/zuhe/cmaup.txt"
+    "NPMGDD/datasets/zuhe/data_before/npass.txt",
+    "NPMGDD/datasets/zuhe/data_before/tcmbank.txt",
+    "NPMGDD/datasets/zuhe/data_before/inflamnat.txt",
+    "NPMGDD/datasets/zuhe/data_before/cmaup.txt"
 ]
+
+output_dir = "NPMGDD/datasets/zuhe/data_remove_invalid/"
+os.makedirs(output_dir, exist_ok=True)
+
 for input_file in input_files:
     output_file = input_file.replace(".txt", "_after.txt")
     

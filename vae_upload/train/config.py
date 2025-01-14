34e3548fe52202a2d801c445e751114e6c315cbd
084317103 <50193282+084317103@users.noreply.github.com>
2025-01-15 04:14:17 +0800

    Create config.py

---
+++ vae_upload/train/config.py
@@ -0,0 +1,131 @@
+import argparse
+import re
+import torch
+
+def add_common_arg(parser):
+    def torch_device(arg):
+        if re.match('^(cuda(:[0-9]+)?|cpu)$', arg) is None:
+            raise argparse.ArgumentTypeError(
+                'Wrong device format: {}'.format(arg)
+            )
+
+        if arg != 'cpu':
+            splited_device = arg.split(':')
+
+            if (not torch.cuda.is_available()) or \
+                    (len(splited_device) > 1 and
+                     int(splited_device[1]) > torch.cuda.device_count()):
+                raise argparse.ArgumentTypeError(
+                    'Wrong device: {} is not available'.format(arg)
+                )
+
+        return arg
+    # Base
+    parser.add_argument('--device',
+                        type=torch_device, default='cuda',
+                        help='Device to run: "cpu" or "cuda:<device number>"')
+    parser.add_argument('--seed',
+                        type=int, default=0,
+                        help='Seed')
+    return parser
+    
+    
+def add_train_args(parser):
+
+    common_arg = parser.add_argument_group('Common')
+    add_common_arg(common_arg)
+    common_arg.add_argument('--train_load', type=str, help='Input data in csv format to train')
+    common_arg.add_argument('--val_load', type=str, help="Input data in csv format for validation")
+    common_arg.add_argument('--model_save', type=str, required=True, default='model.pt', help='Where to save the model')
+    common_arg.add_argument('--save_frequency', type=int, default=6, help='How often to save the model')
+    common_arg.add_argument('--log_file', type=str, required=False, help='Where to save the log')
+    common_arg.add_argument('--config_save', type=str, required=True, help='Where to save the config')
+    common_arg.add_argument('--vocab_save', type=str, help='Where to save the vocab')
+    common_arg.add_argument('--vocab_load', type=str, help='Where to load the vocab; otherwise it will be evaluated')
+    common_arg.add_argument('--model_load', type=str, help='Where to load the model')
+    return parser
+    
+    
+def get_parser(parser=None):
+    if parser is None:
+        parser = argparse.ArgumentParser()
+
+    # Model
+    model_arg = parser.add_argument_group('Model')
+    model_arg.add_argument('--q_cell',
+                           type=str, default='gru', choices=['gru'],
+                           help='Encoder rnn cell type')
+    model_arg.add_argument('--q_bidir',
+                           default=False, action='store_true',
+                           help='If to add second direction to encoder')
+    model_arg.add_argument('--q_d_h',
+                           type=int, default=256,
+                           help='Encoder h dimensionality')
+    model_arg.add_argument('--q_n_layers',
+                           type=int, default=1,
+                           help='Encoder number of layers')
+    model_arg.add_argument('--q_dropout',
+                           type=float, default=0.5,
+                           help='Encoder layers dropout')
+    model_arg.add_argument('--d_cell',
+                           type=str, default='gru', choices=['gru'],
+                           help='Decoder rnn cell type')
+    model_arg.add_argument('--d_n_layers',
+                           type=int, default=3,
+                           help='Decoder number of layers')
+    model_arg.add_argument('--d_dropout',
+                           type=float, default=0,
+                           help='Decoder layers dropout')
+    model_arg.add_argument('--d_z',
+                           type=int, default=128,
+                           help='Latent vector dimensionality')
+    model_arg.add_argument('--d_d_h',
+                           type=int, default=512,
+                           help='Decoder hidden dimensionality')
+    model_arg.add_argument('--freeze_embeddings',
+                           default=False, action='store_true',
+                           help='If to freeze embeddings while training')
+
+    # Train
+    train_arg = parser.add_argument_group('Train')
+    train_arg.add_argument('--n_batch',
+                           type=int, default=64,
+                           help='Batch size')
+    train_arg.add_argument('--clip_grad',
+                           type=int, default=50,
+                           help='Clip gradients to this value')
+    train_arg.add_argument('--kl_start',
+                           type=int, default=0,
+                           help='Epoch to start change kl weight from')
+    train_arg.add_argument('--kl_w_start',
+                           type=float, default=0,
+                           help='Initial kl weight value')
+    train_arg.add_argument('--kl_w_end',
+                           type=float, default=0.05,
+                           help='Maximum kl weight value')
+    train_arg.add_argument('--lr_start',
+                           type=float, default=3 * 1e-4,
+                           help='Initial lr value')
+    train_arg.add_argument('--lr_n_period',
+                           type=int, default=10,
+                           help='Epochs before first restart in SGDR')
+    train_arg.add_argument('--lr_n_restarts',
+                           type=int, default=10,
+                           help='Number of restarts in SGDR')
+    train_arg.add_argument('--lr_n_mult',
+                           type=int, default=1,
+                           help='Mult coefficient after restart in SGDR')
+    train_arg.add_argument('--lr_end',
+                           type=float, default=3 * 1e-4,
+                           help='Maximum lr weight value')
+    train_arg.add_argument('--n_last',
+                           type=int, default=1000,
+                           help='Number of iters to smooth loss calc')
+    train_arg.add_argument('--n_jobs',
+                           type=int, default=1,
+                           help='Number of threads')
+    train_arg.add_argument('--n_workers',
+                           type=int, default=1,
+                           help='Number of workers for DataLoaders')
+    parser = add_train_args(parser)
+    return parser
